9c5ccf7da8f960cd3e2d9ce8fe8275355e5131af
sapumal_p <sapumal_p@epiclanka.net>
2023-08-27 21:18:27 +0530

    Add dummy class for item

---
+++ src/app/dummy-class.ts
@@ -0,0 +1,46 @@
+import { Item } from "./dto/item";
+
+const Dummy_Data : Array<Item> = [
+    new Item("Ioo1",
+            "https://i.ebayimg.com/thumbs/images/g/BPUAAOSwLbNk6M18/s-l300.webp",
+            "White Shirts",
+            2 ,
+            299,
+            5),
+
+    new Item("Ioo2",
+            "https://i.ebayimg.com/thumbs/images/g/~Z0AAOSwA-VeNDRO/s-l300.webp",
+            "White Shirts",
+            4 ,
+            219,
+            10),
+
+    new Item("Ioo1",
+            "https://i.ebayimg.com/thumbs/images/g/XQ4AAOSww1lhnUQh/s-l300.webp",
+            "White Shirts",
+            5,
+            179,
+            15),
+
+    new Item("Ioo1",
+            "https://i.ebayimg.com/thumbs/images/g/htkAAOSwWRdicaTS/s-l300.webp",
+            "White Shirts",
+            1,
+            209,
+            6),
+
+    new Item("Ioo1",
+            "https://i.ebayimg.com/thumbs/images/g/6iUAAOSwwJ5jmpfr/s-l300.webp",
+            "White Shirts",
+            4,
+            180,
+            8),
+
+    new Item("Ioo1",
+            "https://i.ebayimg.com/thumbs/images/g/cZcAAOSwLOZix1J6/s-l300.webp",
+            "White Shirts",
+            2,
+            200,
+            15)
+                            
+]
